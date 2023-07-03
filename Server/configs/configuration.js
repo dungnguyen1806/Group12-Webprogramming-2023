e5f82be510d4d5dc605d32252e8a4908b14b3cb2
DungNguyen <dung.nm194745@sis.hust.edu.vn>
2023-07-03 15:37:36 +0700

    add server side components

---
+++ Server/configs/configuration.js
@@ -0,0 +1,14 @@
+module.exports= {
+  SALT_ROUNDs: +10,
+  DB_URL: "mongodb+srv://tandung10x:tandung@cluster0.pzo36.mongodb.net/JadeHillHomestay?retryWrites=true&w=majority",
+  USER_ADMIN: {
+    username: "admin",
+    password: "admin",
+    role: "admin",
+    more_detail: "default admin"
+  },
+  gmail: {
+    USER: "phonghv006@gmail.com",
+    PASS: "glhvmmxircuzfkty"
+  }
+}
